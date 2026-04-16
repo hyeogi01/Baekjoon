c67c2ef29b34373584bebe5b04741a9b29273746
Lee Chan Hyuk <chan5759@naver.com>
2026-04-16 17:19:19 +0900

    [Bronze V] Title: 이중 반복문, Time: 172 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n <= 10000 ? "Accepted" : "Time limit exceeded");
+    }
+}
